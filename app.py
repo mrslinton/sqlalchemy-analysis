446ae71136fd21353df4e6b2a39c825de6dfdd44
Jennifer Harris <jennifer.v.harris@outlook.com>
2024-08-02 20:36:04 -0400

    completed Climate App

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import numpy as np
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
@@ -24,21 +25,42 @@ def homepage():
     <h3>Available routes:</h3>
 
     <ul>
-        <li>/api/v1.0/precipitation</li>
-        <li>/api/v1.0/stations</li>
-        <li>/api/v1.0/tobs</li>
-        <li>/api/v1.0/[start]</li>
-        <li>/api/v1.0/[start]/[end]</li>
+        <li><a href="/api/v1.0/precipitation">Precipitation</a></li>
+        <li><a href="/api/v1.0/stations">Stations</a></li>
+        <li><a href="/api/v1.0/tobs">Tobs</a></li>
+        <li><a href="/api/v1.0/2016-01-01">Start date can be between 2010-01-01 and 2017-08-23</a></li>
+        <li><a href="/api/v1.0/2016-08-23/2017-08-23">Start and End dates can be between 2010-01-01 and 2017-08-23</a></li>
 
     </ul>
 '''
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
+    return { d:p for d,p in session.query(M.date, M.prcp).filter(M.date>='2016-08-23').all()}
 
+@app.route('/api/v1.0/stations')
+def stations():
+    return { id:loc for id,loc in session.query(S.station, S.name).all()}
 
-    return { d:p for d,p in session.query(M.date, M.prcp).all()}
+@app.route('/api/v1.0/tobs')
+def tobs():
+    return { d:t for d,t in session.query(M.date, M.tobs).all()}
 
+@app.route('/api/v1.0/<start>')
+@app.route('/api/v1.0/<start>/<end>')
+def date_range(start,end='2017-08-23'):
 
+    sel = [func.min(M.tobs),func.avg(M.tobs),func.max(M.tobs)]
+    filter = (M.date>=start)&(M.date<=end)
+
+    result = session.query(*sel).filter(filter).first()
+    
+    return { 
+        '1_Min_Temp':result[0], 
+        '2_Avg_Temp': result[1], 
+        '3_Max_Temp':result[2], 
+        '4_Start_date':start, 
+        '5_End_date':end 
+    }
 
 
